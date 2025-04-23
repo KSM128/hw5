797bc5fd37cc9a93896ce4b554695ad1b683daf3
Jason Gao <gaojason@usc.edu>
2025-04-23 00:09:01 +0000

    added wordle and schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t> shift, size_t nd, size_t nw, size_t row, size_t col);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +38,61 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    const size_t numDay = avail.size(); // avail's row count
+    const size_t numWorker = avail[0].size(); // avail's column count
 
+    // initialize 'empty' schedule
+    vector<Worker_T> tempRow(dailyNeed, INVALID_ID);
+    for(size_t d = 0; d < numDay; d++){
+        sched.push_back(tempRow);
+    }
 
+    // initialize the shift counts for every worker
+    vector<size_t> shift;
+    for(size_t w = 0; w < numWorker; w++){
+        shift.push_back(0);
+    }
+    
+    // call and return recursive helper
+    bool result = scheduleHelper(avail, dailyNeed, maxShifts, sched, shift, numDay, numWorker, 0, 0);
+    return result;
 
 }
 
+// recursive helper, row is simply day# and col is simply Worker#
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, vector<size_t> shift, size_t nd, size_t nw, size_t row, size_t col){
+    // base case: the last day is filled up
+    if(row == nd){
+        return true;
+    }
+
+    // last worker of this day, move to start of next day
+    if(col == dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, shift, nd, nw, row + 1, 0);
+    }
+
+    // loop to go through each worker of this day
+    for(size_t w = 0; w < nw; w++){
+        // if the worker is available today...
+        if(avail[row][w] == 1 &&
+        // if the worker has fewer shifts than maxShift...
+           shift[w] < maxShifts && 
+        // if the worker isn't already working
+           find(sched[row].begin(), sched[row].end(), w) == sched[row].end())
+        {
+            sched[row][col] = w;
+            shift[w]++;
+            
+            // recurse to next worker
+            if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shift, nd, nw, row, col + 1)){
+                return true;
+            }
+
+            // backtrack
+            shift[w]--;
+            sched[row][col] = INVALID_ID;
+        } 
+    }
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+// recursive helper
+void wordleHelper(const string& in, string& curr, int blanks, int pos, int floatCount, const set<string> dict, map<char, int>& floatings, set<string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,70 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    int n = in.size();
+    set<string> results;
 
+    // count number of floating for each character
+    map<char, int> floatings;
+    for(string::const_iterator it = floating.begin(); it != floating.end(); ++it){
+        char c = *it;
+        floatings[c]++;
+    }
+    int floatCount = floating.size();
+
+    // count number of blanks
+    int blanks = 0;
+    for(int i = 0; i < n; i++){
+        if(in[i] == '-'){
+            blanks++;
+        }
+    }
+
+    // call recursive helper
+    string curr = in; // copy of in to avoid modifying in
+    int pos = 0; // current "location" we are checking
+    wordleHelper(in, curr, blanks, pos, floatCount, dict, floatings, results);
+
+    // output the results
+    return results;
 }
 
 // Define any helper functions here
+void wordleHelper(const string& in, string& curr, int blanks, int pos, int floatCount, const set<string> dict, map<char, int>& floatings, set<string>& results){
+    // base case: return if full word; only if floatings are used and word is valid, we add the word to results
+    if(pos == in.size()){
+        if(floatCount == 0 && dict.find(curr) != dict.end()){
+            results.insert(curr);
+        }
+        return;
+    }
+
+    // already known letter, go to next recursive step
+    if(in[pos] != '-'){
+        curr[pos] = in[pos];
+        wordleHelper(in, curr, blanks, pos + 1, floatCount, dict, floatings, results);
+        return;
+    }
+
+    // iterative loop to recurse for floating and free letters
+    for(char c = 'a'; c <= 'z'; c++){
+        map<char, int>::iterator it = floatings.find(c);
+        bool isFloating = false;
+        if(it != floatings.end() && it->second > 0){
+            isFloating = true;
+        }
+        if(isFloating || blanks > floatCount){
+            // make a guess if floating allows, or we have extra unknown letters to skip this floating
+            curr[pos] = c;
+            if(isFloating){
+                // if the floating is used, decrease the floatings and floatCount
+                it->second -= 1; // assume we use this specific floating letter
+                wordleHelper(in, curr, blanks - 1, pos + 1, floatCount - 1, dict, floatings, results);
+                it->second += 1; // after we're finished, 'undo' the changes (matching words are already stored in results)
+            } else {
+                // not floating letter, just proceed and recurse
+                wordleHelper(in, curr, blanks - 1, pos + 1, floatCount, dict, floatings, results);
+            }
+        }
+    }
+}
